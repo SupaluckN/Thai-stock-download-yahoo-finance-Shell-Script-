26c7381153a82eba1035b2af07cdcd515b3639a0
Supaluck Singjan <supalucksingjan@gmail.com>
2022-06-20 11:57:06 +0700

    Add thai stock more

--- thai_stock_download.sh
+++ thai_stock_download.sh
@@ -2,8 +2,16 @@
 from_unix_time=$(date --date='20220601EDT' +"%s")
 to_unix_time=$(date --date='20220605EDT' +"%s")
 
-stock="SCB.BK"
+#stock="SCB.BK"
 
-url="https://query1.finance.yahoo.com/v7/finance/download/${stock}?period1=${from_unix_time}1&period2=${to_unix_time}&interval=1d&events=history&includeAdjustedClose=true"
+for stock in kbank.bk scb.bk ptt.bk; do
 
-echo $url
+    stock=${stock^^}
+    url="https://query1.finance.yahoo.com/v7/finance/download/${stock}?period1=${from_unix_time}1&period2=${to_unix_time}&interval=1d&events=history&includeAdjustedClose=true"
+
+    echo $url
+
+    curl -o "${stock}.csv" "${url}"
+    cat "${stock}.csv"
+
+done
