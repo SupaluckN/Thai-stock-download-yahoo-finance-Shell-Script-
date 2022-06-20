94d3ca3e3add8e6a7073d90880d274df8bb9ec7a
Supaluck Singjan <supalucksingjan@gmail.com>
2022-06-20 12:01:49 +0700

    Add th_banks

--- thai_stock_download.sh
+++ thai_stock_download.sh
@@ -3,8 +3,10 @@ from_unix_time=$(date --date='20220601EDT' +"%s")
 to_unix_time=$(date --date='20220605EDT' +"%s")
 
 #stock="SCB.BK"
+#for stock in kbank.bk scb.bk ptt.bk; do
 
-for stock in kbank.bk scb.bk ptt.bk; do
+
+for stock in $(<th_banks.txt); do
 
     stock=${stock^^}
     url="https://query1.finance.yahoo.com/v7/finance/download/${stock}?period1=${from_unix_time}1&period2=${to_unix_time}&interval=1d&events=history&includeAdjustedClose=true"
